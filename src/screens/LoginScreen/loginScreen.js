f405d17f627337a076cd54e21d9d66d4af2b7f36
Saimoon0311 <shaheer031123@gmail.com>
2022-06-30 18:30:18 +0500

    Traveler screen animated login and signup screen also

--- src/screens/LoginScreen/loginScreen.js
+++ src/screens/LoginScreen/loginScreen.js
@@ -20,8 +20,8 @@ import {ApiGet, ApiPost} from '../../config/helperFunction';
 import {LoginUrl, UserUrl} from '../../config/Urls';
 import {useDispatch} from 'react-redux';
 import types from '../../Redux/type';
-import {showMessage} from 'react-native-flash-message';
 import {errorMessage} from '../../components/NotificationMessage';
+import * as Animatable from 'react-native-animatable';
 
 const LoginScreen = ({route, navigation}) => {
   const disptach = useDispatch();
@@ -117,7 +117,11 @@ const LoginScreen = ({route, navigation}) => {
         <ScrollView
           contentContainerStyle={{paddingBottom: isKeyboardVisible}}
           showsVerticalScrollIndicator={false}>
-          <View style={styles.innerView}>
+          <Animatable.View
+            animation="fadeInUpBig"
+            direction={'normal'}
+            delay={100}
+            style={styles.innerView}>
             <Image
               source={require('../../images/Group680.png')}
               style={{
@@ -126,40 +130,65 @@ const LoginScreen = ({route, navigation}) => {
                 // display: isKeyboardVisible,
               }}
             />
-          </View>
+          </Animatable.View>
           <View style={styles.loginView}>
-            <Text style={styles.mainHeading}>Login</Text>
-            <TextInputCom
-              value={email}
-              onChangeText={email => updateState({email})}
-              inputText="Email"
-              placeholder="mail@gmail.com"
-              onFocus={() => handleInputFocus('email')}
-              onBlur={() => handleInputBlur('email')}
-              isFocused={isFocused.email}
-            />
-            <TextInputCom
-              value={password}
-              onChangeText={password => updateState({password})}
-              inputText="Password"
-              placeholder="*********"
-              onFocus={() => handleInputFocus('password')}
-              onBlur={() => handleInputBlur('password')}
-              secureTextEntry={show ? false : true}
-              eyeIconPress={handleClick}
-              eyeIconName={show ? 'eye-outline' : 'eye-off-outline'}
-              isFocused={isFocused.password}
-              eyeIcon={true}
-            />
-            <TouchableOpacity style={styles.forgotTextView}>
-              <Text
-                style={{
-                  color: color.themeColorDark,
-                }}>
-                Forgot Password?
-              </Text>
-            </TouchableOpacity>
-            <View style={styles.bottomView}>
+            <Animatable.Text
+              animation="fadeInUpBig"
+              direction={'normal'}
+              delay={200}
+              style={styles.mainHeading}>
+              Login
+            </Animatable.Text>
+            <Animatable.View
+              animation="fadeInUpBig"
+              direction={'normal'}
+              delay={300}>
+              <TextInputCom
+                value={email}
+                onChangeText={email => updateState({email})}
+                inputText="Email"
+                placeholder="mail@gmail.com"
+                onFocus={() => handleInputFocus('email')}
+                onBlur={() => handleInputBlur('email')}
+                isFocused={isFocused.email}
+              />
+            </Animatable.View>
+            <Animatable.View
+              animation="fadeInUpBig"
+              direction={'normal'}
+              delay={400}>
+              <TextInputCom
+                value={password}
+                onChangeText={password => updateState({password})}
+                inputText="Password"
+                placeholder="*********"
+                onFocus={() => handleInputFocus('password')}
+                onBlur={() => handleInputBlur('password')}
+                secureTextEntry={show ? false : true}
+                eyeIconPress={handleClick}
+                eyeIconName={show ? 'eye-outline' : 'eye-off-outline'}
+                isFocused={isFocused.password}
+                eyeIcon={true}
+              />
+            </Animatable.View>
+            <Animatable.View
+              animation="fadeInUpBig"
+              direction={'normal'}
+              delay={500}>
+              <TouchableOpacity style={styles.forgotTextView}>
+                <Text
+                  style={{
+                    color: color.themeColorDark,
+                  }}>
+                  Forgot Password?
+                </Text>
+              </TouchableOpacity>
+            </Animatable.View>
+            <Animatable.View
+              animation="fadeInUpBig"
+              direction={'normal'}
+              delay={600}
+              style={styles.bottomView}>
               {LoginType == 'Traveller' ? (
                 <View style={{flexDirection: 'row'}}>
                   <Text style={styles.newUserText}>New User ? </Text>
@@ -177,7 +206,7 @@ const LoginScreen = ({route, navigation}) => {
                 text="Submit"
                 height={hp('4.5')}
               />
-            </View>
+            </Animatable.View>
           </View>
         </ScrollView>
       </ImageBackground>
